8659f10b1790387035ca39dd12827009e5dcd97d
CharlieDeltaZA <calvin@diogo.co.za>
2018-05-27 18:54:09 +0200

    Add some things...

--- bot.py
+++ bot.py
@@ -15,3 +15,18 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+
+
+bot = commands.Bot(command_prefix='!', description="Charlie's Bitch")
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+
+bot.run(token)
