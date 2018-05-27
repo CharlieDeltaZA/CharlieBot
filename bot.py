4c6fa2d4439dafaa24598ef493c8797cd83b963d
CharlieDeltaZA <calvin@diogo.co.za>
2018-05-27 20:04:40 +0200

    .

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
-    print('------')
     await bot.change_presence(game=discord.Game(name='!help'))
+    print('------')
 
 
 
